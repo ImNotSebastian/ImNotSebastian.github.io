a833653a8f6d73bf26e60064c53f3c62def8949f
VenixFire <aliecbot@gmail.com>
2024-03-21 14:00:47 -0700

    Moved to XAMPP folder, changed to php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
+
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
 <link rel="stylesheet" href="../style/styles.css">
 <title>CSS Template</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="refresh" content="0; url=https://ImNotSebastian.github.io/public/index.html">
+<meta http-equiv="refresh" content="0; url=public/index.php">
 </head>
 <body>
 </body>
